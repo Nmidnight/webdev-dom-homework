bc2c42dd4b38380f83194168dde005361a48693d
Nmidnight <bynanamid46@gmail.com>
2025-07-24 12:14:18 +0300

    hw done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 'use strict'
 import { addComment, likeComment, quoteComment } from './eventListener.js'
-import { renderComments } from './renderFn.js'
+import { fetchCommentsToServer } from './comments.js'
 
-renderComments()
+const loader = document.createElement('li')
+loader.classList.add('loader')
+loader.innerHTML = 'Подождите немного, комментарии загружаются...'
+
+document.querySelector('.comments').appendChild(loader)
+
+fetchCommentsToServer()
+
 addComment()
 likeComment()
 quoteComment()
