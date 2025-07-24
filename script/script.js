bc822f760013d7b0f55622fddc43722c0c6e3477
Nmidnight <bynanamid46@gmail.com>
2025-07-24 15:51:41 +0300

    fixed

--- script/script.js
+++ script/script.js
@@ -2,12 +2,6 @@
 import { addComment, likeComment, quoteComment } from './eventListener.js'
 import { fetchCommentsToServer } from './comments.js'
 
-const loader = document.createElement('li')
-loader.classList.add('loader')
-loader.innerHTML = 'Подождите немного, комментарии загружаются...'
-
-document.querySelector('.comments').appendChild(loader)
-
 fetchCommentsToServer()
 
 addComment()
